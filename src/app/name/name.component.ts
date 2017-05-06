06aa4a3580f90597d223016d8d24ccd171846914
Alex Miller <apmiller108@yahoo.com>
2017-05-06 13:55:23 -0400

    Add initials section that only shows for browsers that do not support css clip-path property

--- src/app/name/name.component.ts
+++ src/app/name/name.component.ts
@@ -7,10 +7,23 @@ import { Component } from '@angular/core';
 })
 
 export class NameComponent {
-    public isChrome: boolean;
-    public userAgent: string;
-    constructor() {
-        this.userAgent = navigator.userAgent;
-        this.isChrome = /Chrome/.test(this.userAgent);
-    }
+  public supportsClipPath: boolean;
+  public photo: string;
+  private userAgent: string;
+  private isChrome: boolean;
+  private isFirefox: boolean;
+  private isSafari: boolean;
+  private isEdge: boolean;
+  constructor() {
+    this.userAgent = navigator.userAgent;
+    this.isEdge = /Edge/.test(this.userAgent);
+    this.isChrome = /Chrome/.test(this.userAgent) && !this.isEdge;
+    this.isFirefox = /Firefox/.test(this.userAgent);
+    this.isSafari = /Safari/.test(this.userAgent)
+      && !this.isFirefox
+      && !this.isChrome
+      && !this.isEdge;
+    this.supportsClipPath = this.isChrome;
+    this.photo = require('../../assets/images/alex_miller.jpg');
+  }
 }
